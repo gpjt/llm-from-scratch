9a113dbb671d924ed1bb66d4aa793f43845b695a
Giles Thomas <giles@giles.net>
2025-08-20 21:59:05 +0100

    Moved model config into its own file

--- initial_test.py
+++ initial_test.py
@@ -1,17 +1,7 @@
 import torch
 
 from gpt import GPTModel
-
-
-GPT_CONFIG_124M = {
-    "vocab_size": 50257,
-    "context_length": 1024,
-    "emb_dim": 768,
-    "n_heads": 12,
-    "n_layers": 12,
-    "drop_rate": 0.1,
-    "qkv_bias": False
-}
+from model_config import GPT_CONFIG_124M
 
 
 torch.manual_seed(123)
