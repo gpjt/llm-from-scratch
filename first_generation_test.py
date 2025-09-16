ba974f08debb681c71183c131aa579d0d639d9b5
Giles Thomas <giles@giles.net>
2025-09-16 17:48:17 +0100

    Factored generate_text_simple out into its own module

--- first_generation_test.py
+++ first_generation_test.py
@@ -1,6 +1,7 @@
 import tiktoken
 import torch
 
+from generate_text_simple import generate_text_simple
 from gpt import GPTModel
 from model_config import GPT_CONFIG_124M
 
@@ -8,21 +9,6 @@ from model_config import GPT_CONFIG_124M
 torch.manual_seed(123)
 tokenizer = tiktoken.get_encoding("gpt2")
 model = GPTModel(GPT_CONFIG_124M)
-
-
-def generate_text_simple(model, idx, max_new_tokens, context_size):
-    for _ in range(max_new_tokens):
-        idx_cond = idx[:, -context_size:]
-        with torch.no_grad():
-            logits = model(idx_cond)
-
-        logits = logits[:, -1, :]
-        probs = torch.softmax(logits, dim=-1)
-        idx_next = torch.argmax(probs, dim=-1, keepdim=True)
-        idx = torch.cat((idx, idx_next), dim=-1)
-
-    return idx
-
 
 start_context = "Hello, I am"
 encoded = tokenizer.encode(start_context)
