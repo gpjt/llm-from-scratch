3a8957261439a56e2f06f0a138c5ac8c0d76ea69
Giles Thomas <giles@giles.net>
2025-10-15 20:16:45 +0100

    Code to get to the end of ch5

--- train_per_book.py
+++ train_per_book.py
@@ -5,6 +5,7 @@ from dataloader import create_dataloader_v1
 from gpt import GPTModel
 from model_config import GPT_CONFIG_124M
 from second_generation_test import text_to_token_ids, token_ids_to_text
+from generate_text import generate
 from generate_text_simple import generate_text_simple
 
 
@@ -110,35 +111,6 @@ def evaluate_model(model, train_loader, val_loader, device, eval_iter):
         )
     model.train()
     return train_loss, val_loss
-
-
-def generate(
-    model, idx, max_new_tokens, context_size,
-    temperature=0.0, top_k=None, eos_id=None
-):
-    for _ in range(max_new_tokens):
-        idx_cond = idx[:, -context_size:]
-        with torch.no_grad():
-            logits = model(idx_cond)
-        logits = logits[:, -1, :]
-        if top_k is not None:
-            top_logits, _ = torch.topk(logits, top_k)
-            min_val = top_logits[:, -1]
-            logits = torch.where(
-                logits < min_val,
-                torch.tensor(float('-inf')).to(logits.device),
-                logits
-            )
-        if temperature > 0.0:
-            logits = logits / temperature
-            probs = torch.softmax(logits, dim=-1)
-            idx_next = torch.multinomial(probs, num_samples=1)
-        else:
-            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
-        if idx_next == eos_id:
-            break
-        idx = torch.cat((idx, idx_next), dim=-1)
-    return idx
 
 
 def generate_and_print_sample(model, tokenizer, device, start_context):
