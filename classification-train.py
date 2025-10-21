9339ab4a554434ff6c16ff8c9374a59179c94c2b
Giles Thomas <giles@giles.net>
2025-10-22 00:59:29 +0100

    Continuing with classification

---
+++ classification-train.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import tiktoken
+import torch
+from torch.utils.data import Dataset
+
+
+class SpamDataset(Dataset):
+
+    def __init__(
+        self, csv_file, tokenizer, max_length=None, pad_token_id=50256
+    ):
+        self.data = pd.read_csv(csv_file)
+
+        self.encoded_texts = [
+            tokenizer.encode(text) for text in self.data["Text"]
+        ]
+
+        if max_length is None:
+            self.max_length = self._longest_encoded_length()
+        else:
+            self.max_length = max_length
+
+            self.encoded_texts = [
+                encoded_text[:self.max_length]
+                for encoded_text in self.encoded_texts
+            ]
+
+        self.encoded_texts = [
+            encoded_text + [pad_token_id] * (self.max_length - len(encoded_text))
+            for encoded_text in self.encoded_texts
+        ]
+
+
+    def __getitem__(self, ix):
+        encoded = self.encoded_texts[ix]
+        label = self.data.iloc[ix]["Label"]
+
+        return (
+            torch.tensor(encoded, dtype=torch.long),
+            torch.tensor(label, dtype=torch.long)
+        )
+
+
+    def __len__(self):
+        return len(self.data)
+
+
+    def _longest_encoded_length(self):
+        max_length = 0
+        for encoded_text in self.encoded_texts:
+            encoded_length = len(encoded_text)
+            if encoded_length > max_length:
+                max_length = encoded_length
+        return max_length
+
+
+
+
+def main():
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    train_dataset = SpamDataset(
+        csv_file="classification-train.csv",
+        max_length=None,
+        tokenizer = tokenizer
+    )
+    print(train_dataset.max_length)
