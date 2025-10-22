48a54b8b6829a4318bea08a4675e179e898b971c
Giles Thomas <giles@giles.net>
2025-10-22 01:20:58 +0100

    Initialise our DataLoaders

--- classification-train.py
+++ classification-train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import tiktoken
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Dataset
 
 
 class SpamDataset(Dataset):
@@ -62,6 +62,57 @@ def main():
     train_dataset = SpamDataset(
         csv_file="classification-train.csv",
         max_length=None,
-        tokenizer = tokenizer
+        tokenizer=tokenizer
     )
     print(train_dataset.max_length)
+    val_dataset = SpamDataset(
+        csv_file="classification-validation.csv",
+        max_length=train_dataset.max_length,
+        tokenizer=tokenizer
+    )
+    test_dataset = SpamDataset(
+        csv_file="classification-test.csv",
+        max_length=train_dataset.max_length,
+        tokenizer=tokenizer
+    )
+
+    num_workers = 0
+    batch_size = 8
+    torch.manual_seed(123)
+
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=True
+    )
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=False
+    )
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=False
+    )
+
+    for input_batch, target_batch in train_loader:
+        pass
+    print("Input batch dimensions:", input_batch.shape)
+    print("Label batch dimensions:", target_batch.shape)
+
+    print(f"{len(train_loader)} training batches")
+    print(f"{len(val_loader)} validation batches")
+    print(f"{len(test_loader)} test batches")
+
+
+
+
+if __name__ == "__main__":
+    main()
