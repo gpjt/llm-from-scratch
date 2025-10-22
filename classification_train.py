287339f99581e511feece9dcc6cbb33f0a224216
Giles Thomas <giles@giles.net>
2025-10-22 21:30:04 +0100

    Final classification -- not such good results, boo

--- classification_train.py
+++ classification_train.py
@@ -203,6 +203,29 @@ def plot_values(
 
 
 
+def classify_review(
+    text, model, tokenizer, device, max_length=None, pad_token_id=50256
+):
+    model.eval()
+
+    input_ids = tokenizer.encode(text)
+    supported_context_length = model.pos_emb.weight.shape[1]
+
+    input_ids = input_ids[:min(max_length, supported_context_length)]
+    input_ids += [pad_token_id] * (max_length - len(input_ids))
+
+    input_tensor = torch.tensor(input_ids, device=device).unsqueeze(0)
+
+    with torch.no_grad():
+        logits = model(input_tensor)[:, -1, :]
+        print(torch.softmax(logits, dim=-1))
+    predicted_label = torch.argmax(logits, dim=-1).item()
+    print(predicted_label)
+
+    return "spam" if predicted_label == 1 else "not spam"
+
+
+
 
 def main():
     tokenizer = tiktoken.get_encoding("gpt2")
@@ -411,6 +434,20 @@ def main():
     print(f"Training accuracy: {train_accuracy * 100:.2f}%")
     print(f"Validation accuracy: {val_accuracy * 100:.2f}%")
     print(f"Test accuracy: {test_accuracy * 100:.2f}%")
+
+    text_1 = (
+        "You are a winner you have been specially"
+        " selected to receive $1000 cash or a $2000 reward"
+    )
+    print(classify_review(text_1, model, tokenizer, device, max_length=train_dataset.max_length))
+
+    text_2 = (
+        "Hey, just wanted to check if we're still on"
+        " for dinner tonight? Let me know!"
+    )
+    print(classify_review(text_2, model, tokenizer, device, max_length=train_dataset.max_length))
+
+
 
 
 
