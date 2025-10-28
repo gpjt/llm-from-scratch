660d7bec8f3f17166338e3e8a39a88cb94c18b2e
Giles Thomas <giles@giles.net>
2025-10-28 18:03:47 +0000

    WIP continues

--- ift-from-book.py
+++ ift-from-book.py
@@ -95,6 +95,46 @@ def custom_collate_draft_2(
     return inputs_tensor, targets_tensor
 
 
+def custom_collate_fn(
+    batch, pad_token_id=50256, ignore_index=-100,
+    allowed_max_length=None, device="cpu"
+):
+    batch_max_length = max(len(item) + 1 for item in batch)
+    inputs_lst = []
+    targets_lst = []
+
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+
+        padded = (
+            new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        )
+
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+
+        mask = targets == pad_token_id
+        print(mask)
+        indices = torch.nonzero(mask)
+        print(indices)
+        indices = indices.squeeze()
+        print(indices)
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+
+
 def main():
     file_path = "instruction-data.json"
     url = (
@@ -139,6 +179,11 @@ def main():
     inputs, targets = custom_collate_draft_2(batch)
     print(inputs)
     print(targets)
+
+    inputs, targets = custom_collate_fn(batch)
+    print(inputs)
+    print(targets)
+
 
 
 
