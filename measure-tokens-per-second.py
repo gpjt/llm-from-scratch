e24df6aad31f514b320aa0c19531470055213fe5
Giles Thomas <giles@giles.net>
2025-11-06 19:37:53 +0000

    First cut of script to measure tokens/sec

---
+++ measure-tokens-per-second.py
@@ -0,0 +1,84 @@
+import time
+
+import tiktoken
+import torch
+from datasets import load_dataset
+
+from gpt import GPTModel
+
+NUM_BATCHES = 100
+BATCH_SIZE = 5
+SEQ_LENGTH = 1024
+
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    big_train_params = {
+        "vocab_size": 50257,
+        "context_length": 1024,
+        "emb_dim": 768,
+        "n_heads": 12,
+        "n_layers": 12,
+        "drop_rate": 0.1,
+        "qkv_bias": False
+    }
+
+    model = GPTModel(big_train_params)
+    model.to(device)
+
+    ds = load_dataset(
+        "parquet",
+        data_files="./fineweb/sample/10BT/*.parquet",
+        split="train"
+    )
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+    all_tokens = []
+    train_tokens = (NUM_BATCHES * BATCH_SIZE * SEQ_LENGTH) + 1
+    for element in ds:
+        text = element["text"]
+        tokens = tokenizer.encode(text)
+        all_tokens += tokens
+        if len(all_tokens) >= train_tokens:
+            break
+    all_tokens = all_tokens[:train_tokens]
+    input_token_count = train_tokens - 1
+    print(f"Training on {input_token_count:,} input tokens")
+
+    batches = []
+    for batch in range(NUM_BATCHES):
+        start = batch * BATCH_SIZE * SEQ_LENGTH
+        end = start + BATCH_SIZE * SEQ_LENGTH
+        inputs = all_tokens[start:end]
+        outputs = all_tokens[start + 1:end + 1]
+        input_tensor = torch.tensor(inputs).reshape(BATCH_SIZE, SEQ_LENGTH)
+        output_tensor = torch.tensor(outputs).reshape(BATCH_SIZE, SEQ_LENGTH)
+        batches.append((input_tensor, output_tensor))
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=0.0004, weight_decay=0.1
+    )
+
+    start = time.time()
+    for inputs, outputs in batches:
+        inputs = inputs.to(device)
+        outputs = outputs.to(device)
+        optimizer.zero_grad()
+        logits = model(inputs)
+        loss = torch.nn.functional.cross_entropy(
+            logits.flatten(0, 1), outputs.flatten()
+        )
+        loss.backward()
+        optimizer.step()
+    end = time.time()
+
+    seconds = end - start
+
+    print(f"Done, {input_token_count:,} tokens done in {seconds:.4f}s.")
+    print(f"Tokens per second: {int(input_token_count / seconds):,}")
+
+
+if __name__ == "__main__":
+    main()
